99224fbdc45aed5d541f8246c1383685ec0584d3
Gourab <gourabbistu2002@gmail.com>
2025-05-17 01:28:55 +0530

    cors update

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import pickle
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": "http://localhost:5173"}})
+CORS(app, resources={r"/predict": {"origins": "https://crops-detection-app.vercel.app/"}})
 
 # Load the trained model
 with open('model.pkl', 'rb') as f:
